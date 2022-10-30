2f4a907cf31436892053d32b50ab66fec016da29
Adeeba Khan <i190626@nu.edu.pk>
2022-10-30 23:15:20 +0500

    Updated

--- app/src/main/java/com/ass2/i190438_i190626/SignUp.java
+++ app/src/main/java/com/ass2/i190438_i190626/SignUp.java
@@ -180,6 +180,7 @@ public class SignUp extends AppCompatActivity {
             email.requestFocus();
             checks = false;
         }
+        // returns true if all checks satisfied
         return checks;
     }
 }
